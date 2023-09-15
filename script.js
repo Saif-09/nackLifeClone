e1967277b6c2fb7d2cf124a07fb06ddf50fbe817
Saif-09 <saifmd238@gmail.com>
2023-09-15 06:01:21 +0530

    updated indentation

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     const nav = document.querySelector('.nav');
-    const navListItems = document.querySelectorAll('.nav-list ul li'); 
-    const logo = document.querySelector('.logo-svg'); 
-    
+    const navListItems = document.querySelectorAll('.nav-list ul li');
+    const logo = document.querySelector('.logo-svg');
+
     if (window.scrollY > 0) {
         nav.classList.add('nav-scrolled');
-    
-        navListItems.forEach(function(item) {
+
+        navListItems.forEach(function (item) {
             item.style.color = 'black';
         });
 
         logo.setAttribute('fill', 'black');
     } else {
         nav.classList.remove('nav-scrolled');
-        navListItems.forEach(function(item) {
+        navListItems.forEach(function (item) {
             item.style.color = 'white';
         });
         logo.setAttribute('fill', 'white');
@@ -46,21 +46,21 @@ const delay = 3000
 
 boxWrapper.style.setProperty('--per-view', perView)
 for (let i = 0; i < perView; i++) {
-  boxWrapper.insertAdjacentHTML('beforeend', box[i].outerHTML)
+    boxWrapper.insertAdjacentHTML('beforeend', box[i].outerHTML)
 }
 
 let autoScroll = setInterval(scrolling, delay)
 
-function scrolling(){
-  totalScroll++
-  if(totalScroll=== boxLength+1){
-    clearInterval(autoScroll)
-    totalScroll=1
-    boxWrapper.style.transition = '0s'
-    boxWrapper.style.left = '0'
-    autoScroll = setInterval(scrolling, delay)
-  }
-  const widthEl = document.querySelector('.box-wrapper > :first-child').offsetWidth + 24
-  boxWrapper.style.left = `-${totalScroll * widthEl}px`
-  boxWrapper.style.transition = '.3s'
+function scrolling() {
+    totalScroll++
+    if (totalScroll === boxLength + 1) {
+        clearInterval(autoScroll)
+        totalScroll = 1
+        boxWrapper.style.transition = '0s'
+        boxWrapper.style.left = '0'
+        autoScroll = setInterval(scrolling, delay)
+    }
+    const widthEl = document.querySelector('.box-wrapper > :first-child').offsetWidth + 24
+    boxWrapper.style.left = `-${totalScroll * widthEl}px`
+    boxWrapper.style.transition = '.3s'
 }
